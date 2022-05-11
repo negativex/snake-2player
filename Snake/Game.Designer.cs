8b3150b5ed1c6da5394df996cdb127dd1315dff2
negativex <63691219+negativex@users.noreply.github.com>
2022-05-11 14:41:10 +0700

    Add files via upload

---
+++ Snake/Game.Designer.cs
@@ -0,0 +1,208 @@
+﻿namespace Snake
+{
+    partial class frmGame
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmGame));
+            this.mnuMenu = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.newGameToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.picCanvas = new System.Windows.Forms.PictureBox();
+            this.lblLScore = new System.Windows.Forms.Label();
+            this.lblScore = new System.Windows.Forms.Label();
+            this.tmrGameSpeed = new System.Windows.Forms.Timer(this.components);
+            this.lblGameOver = new System.Windows.Forms.Label();
+            this.btnPlay = new System.Windows.Forms.Button();
+            this.lblTimer = new System.Windows.Forms.Label();
+            this.tmrTimer = new System.Windows.Forms.Timer(this.components);
+            this.btnExit = new System.Windows.Forms.Button();
+            this.tmrTimer2 = new System.Windows.Forms.Timer(this.components);
+            this.lbldiemp2 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.mnuMenu.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.picCanvas)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // mnuMenu
+            // 
+            this.mnuMenu.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.mnuMenu.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.helpToolStripMenuItem});
+            resources.ApplyResources(this.mnuMenu, "mnuMenu");
+            this.mnuMenu.Name = "mnuMenu";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.newGameToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            resources.ApplyResources(this.fileToolStripMenuItem, "fileToolStripMenuItem");
+            // 
+            // newGameToolStripMenuItem
+            // 
+            this.newGameToolStripMenuItem.Name = "newGameToolStripMenuItem";
+            resources.ApplyResources(this.newGameToolStripMenuItem, "newGameToolStripMenuItem");
+            this.newGameToolStripMenuItem.Click += new System.EventHandler(this.newGameToolStripMenuItem_Click);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            resources.ApplyResources(this.exitToolStripMenuItem, "exitToolStripMenuItem");
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // helpToolStripMenuItem
+            // 
+            this.helpToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.aboutToolStripMenuItem});
+            this.helpToolStripMenuItem.Name = "helpToolStripMenuItem";
+            resources.ApplyResources(this.helpToolStripMenuItem, "helpToolStripMenuItem");
+            // 
+            // aboutToolStripMenuItem
+            // 
+            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            resources.ApplyResources(this.aboutToolStripMenuItem, "aboutToolStripMenuItem");
+            this.aboutToolStripMenuItem.Click += new System.EventHandler(this.aboutToolStripMenuItem_Click);
+            // 
+            // picCanvas
+            // 
+            resources.ApplyResources(this.picCanvas, "picCanvas");
+            this.picCanvas.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.picCanvas.Name = "picCanvas";
+            this.picCanvas.TabStop = false;
+            this.picCanvas.Paint += new System.Windows.Forms.PaintEventHandler(this.picCanvas_Paint);
+            // 
+            // lblLScore
+            // 
+            resources.ApplyResources(this.lblLScore, "lblLScore");
+            this.lblLScore.Name = "lblLScore";
+            // 
+            // lblScore
+            // 
+            resources.ApplyResources(this.lblScore, "lblScore");
+            this.lblScore.Name = "lblScore";
+            // 
+            // lblGameOver
+            // 
+            resources.ApplyResources(this.lblGameOver, "lblGameOver");
+            this.lblGameOver.Name = "lblGameOver";
+            // 
+            // btnPlay
+            // 
+            resources.ApplyResources(this.btnPlay, "btnPlay");
+            this.btnPlay.Name = "btnPlay";
+            this.btnPlay.UseVisualStyleBackColor = true;
+            this.btnPlay.Click += new System.EventHandler(this.btnPlay_Click);
+            // 
+            // lblTimer
+            // 
+            resources.ApplyResources(this.lblTimer, "lblTimer");
+            this.lblTimer.Name = "lblTimer";
+            // 
+            // btnExit
+            // 
+            resources.ApplyResources(this.btnExit, "btnExit");
+            this.btnExit.Name = "btnExit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // lbldiemp2
+            // 
+            resources.ApplyResources(this.lbldiemp2, "lbldiemp2");
+            this.lbldiemp2.Name = "lbldiemp2";
+            // 
+            // label2
+            // 
+            resources.ApplyResources(this.label2, "label2");
+            this.label2.Name = "label2";
+            // 
+            // label3
+            // 
+            resources.ApplyResources(this.label3, "label3");
+            this.label3.Name = "label3";
+            // 
+            // frmGame
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lbldiemp2);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.lblGameOver);
+            this.Controls.Add(this.lblTimer);
+            this.Controls.Add(this.btnPlay);
+            this.Controls.Add(this.lblScore);
+            this.Controls.Add(this.lblLScore);
+            this.Controls.Add(this.picCanvas);
+            this.Controls.Add(this.mnuMenu);
+            this.KeyPreview = true;
+            this.MainMenuStrip = this.mnuMenu;
+            this.Name = "frmGame";
+            this.Load += new System.EventHandler(this.frmGame_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.frmGame_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.frmGame_KeyUp);
+            this.mnuMenu.ResumeLayout(false);
+            this.mnuMenu.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.picCanvas)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        
+        #endregion
+
+        private System.Windows.Forms.MenuStrip mnuMenu;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem newGameToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+        private System.Windows.Forms.PictureBox picCanvas;
+        private System.Windows.Forms.Label lblLScore;
+        private System.Windows.Forms.Label lblScore;
+        private System.Windows.Forms.Timer tmrGameSpeed;
+        private System.Windows.Forms.Label lblGameOver;
+        private System.Windows.Forms.Button btnPlay;
+        private System.Windows.Forms.Label lblTimer;
+        private System.Windows.Forms.Timer tmrTimer;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Timer tmrTimer2;
+        private System.Windows.Forms.Label lbldiemp2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Snake/Game.cs
@@ -0,0 +1,385 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Snake
+{
+    public partial class frmGame : Form
+    {
+        // snake is a list implementation.
+        private List<Circle> snake = new List<Circle>();
+        private List<Circle> snake2 = new List<Circle>();
+        // food are circles.
+        private Circle food = new Circle();
+
+        public frmGame()
+        {
+            InitializeComponent();
+        }
+
+        private void startGame()
+        {
+            // disable the controls.
+            lblGameOver.Visible = false;
+            btnPlay.Enabled = false;
+            newGameToolStripMenuItem.Enabled = false;
+            // initialize with default settings.
+            new Settings();
+            new SettingsP2();
+            // set game timer.
+            tmrTimer.Interval = 1000 / Settings.speed;
+            tmrTimer2.Interval = 1000 / Settings.speed;
+            tmrTimer.Tick += updateScreen;
+            tmrTimer2.Tick += updateScreen2;
+            tmrTimer.Start();
+            tmrTimer2.Start();
+            // remove the whole snake.
+            snake.Clear();
+            snake2.Clear();
+            // create the snake head node.
+            Circle snakeHead = new Circle() { x = 6, y = 10 };
+            Circle snakeHead2 = new Circle() { x = 18, y = 10 };
+            // add the head to the body.
+            snake.Add(snakeHead);
+            snake2.Add(snakeHead2);
+            lblScore.Text = Settings.score.ToString();
+            lbldiemp2.Text = SettingsP2.score.ToString();
+            generateFood();
+        }
+
+        /// <summary>
+        /// Places random food objects in the game.
+        /// </summary>
+        private void generateFood()
+        {
+            // get the max x and y positions.
+            int maxXPos = (picCanvas.Size.Width / Settings.width);
+            int maxYPos = (picCanvas.Size.Height / Settings.height);
+
+            // create a random food object.
+            Random random = new Random();
+            food = new Circle() { x = random.Next(0, maxXPos),
+                y = random.Next(0, maxYPos) };
+        }
+
+        private void updateScreen(object sender, EventArgs e)
+        {
+            // check for game over.
+            if (Settings.gameOver) {
+                // check if Enter is pressed.
+                if (Input.KeyPressed(Keys.Return)) { startGame(); }
+            }
+            else {
+
+                Settings.duration += 1;
+
+                if (Input.KeyPressed(Keys.D) && Settings.direction != Direction.Left)
+                    Settings.direction = Direction.Right;
+                else if (Input.KeyPressed(Keys.A) && Settings.direction != Direction.Right)
+                    Settings.direction = Direction.Left;
+                else if (Input.KeyPressed(Keys.W) && Settings.direction != Direction.Down)
+                    Settings.direction = Direction.Up;
+                else if (Input.KeyPressed(Keys.S) && Settings.direction != Direction.Up)
+                    Settings.direction = Direction.Down;
+
+                movePlayer();
+                lblTimer.Text = TimeSpan.FromSeconds(Settings.duration / Settings.speed).ToString();
+                lblScore.Text = Settings.score.ToString();
+            }
+
+            picCanvas.Refresh();
+        }
+        private void updateScreen2(object sender, EventArgs e)
+        {
+            // check for game over.
+            if (SettingsP2.gameOver)
+            {
+                // check if Enter is pressed.
+                if (Input.KeyPressed(Keys.Return)) { startGame(); }
+            }
+            else
+            {
+
+                SettingsP2.duration += 1;
+
+                if (Input.KeyPressed(Keys.L) && SettingsP2.direction != Direction.Left)
+                    SettingsP2.direction = Direction.Right;
+                else if (Input.KeyPressed(Keys.J) && SettingsP2.direction != Direction.Right)
+                    SettingsP2.direction = Direction.Left;
+                else if (Input.KeyPressed(Keys.I) && SettingsP2.direction != Direction.Down)
+                    SettingsP2.direction = Direction.Up;
+                else if (Input.KeyPressed(Keys.K) && SettingsP2.direction != Direction.Up)
+                    SettingsP2.direction = Direction.Down;
+
+                movePlayer2();
+                //lblTimer.Text = TimeSpan.FromSeconds(SettingsP2.duration / SettingsP2.speed).ToString();
+                lbldiemp2.Text = SettingsP2.score.ToString();
+            }
+
+            picCanvas.Refresh();
+        }
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void picCanvas_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics canvas = e.Graphics;
+
+            if (Settings.gameOver)
+            {
+                string msg = "Ket thuc,\nDiem cua nguoi choi 1 la: " + Settings.score + "." + "\nDiem cua nguoi choi 2 la: " +SettingsP2.score + ".";
+                msg += "\nNhan Enter de choi lai.";
+                lblGameOver.Text = msg;
+                lblGameOver.Visible = true;
+            }
+            else
+            {
+                Brush snakeColour;
+                // draw the snake.
+                for (int i = 0; i < snake.Count; ++i)
+                {
+                    // draw and colour head black.
+                    if (i == 0)
+                        snakeColour = Brushes.Black;
+                    // draw and colour rest of body green.
+                    else
+                        snakeColour = Brushes.Green;
+                    // draw the whole snake.
+                    canvas.FillEllipse(snakeColour,
+                        new Rectangle(snake[i].x * Settings.width,
+                        snake[i].y * Settings.height,
+                        Settings.width, Settings.height));
+                    // draw food.
+                    canvas.FillEllipse(Brushes.Red,
+                        new Rectangle(food.x * Settings.width,
+                        food.y * Settings.height, Settings.width,
+                        Settings.height));
+                    
+                    Brush snakeColour2;
+                    //draw the snake.
+                    for (int q = 0; q < snake2.Count; ++q)
+                    {
+                        // draw and colour head black.
+                        if (q == 0)
+                            snakeColour2 = Brushes.Black;
+                        // draw and colour rest of body green.
+                        else
+                            snakeColour2 = Brushes.Blue;
+                        // draw the whole snake.
+                        canvas.FillEllipse(snakeColour2,
+                            new Rectangle(snake2[q].x * SettingsP2.width,
+                            snake2[q].y * SettingsP2.height,
+                            SettingsP2.width, SettingsP2.height));
+                        //draw food.
+                        canvas.FillEllipse(Brushes.Red,
+                            new Rectangle(food.x * SettingsP2.width,
+                            food.y * SettingsP2.height, SettingsP2.width,
+                            SettingsP2.height));
+                    }
+                }
+            }
+
+        }
+        
+        
+        private void movePlayer()
+        {
+            for (int i = snake.Count - 1; i >= 0; i--) {
+                // move the head.
+                if (i == 0) {
+                    switch (Settings.direction) {
+                        case Direction.Right:
+                            snake[i].x++;
+                            break;
+                        case Direction.Left:
+                            snake[i].x--;
+                            break;
+                        case Direction.Up:
+                            snake[i].y--;
+                            break;
+                        case Direction.Down:
+                            snake[i].y++;
+                            break;
+                    }
+                    // get max X and Y positions.
+                    int maxXPos = (picCanvas.Size.Width / Settings.width);
+                    int maxYPos = (picCanvas.Size.Height / Settings.height);
+                    // detect collision with game boundaries.
+                    if (snake[i].x < 0 || snake[i].y < 0
+                        || snake[i].x >= maxXPos || snake[i].y >= maxYPos) {
+                        die();
+                    }
+                    // detect collision with body.
+                    for (int j = 1; j < snake.Count; ++j) {
+                        if ((snake[i].x == snake[j].x && snake[i].y == snake[j].y)) {
+                            die();
+                        }
+                    }
+                    for (int k = 1; k < snake2.Count; ++k)
+                    {
+                        if ((snake[0].x == snake2[k].x && snake[0].y == snake2[k].y))
+                        {
+                            die();
+                        }
+                    }
+                    // detect collision with food.
+                    if (snake[i].x == food.x && snake[i].y == food.y) {
+                        eat();
+                    }
+                }
+                // move the rest of the body.
+                else {
+                    // set current node to previous node's position.
+                    snake[i].x = snake[i - 1].x;
+                    snake[i].y = snake[i - 1].y;
+                }
+
+            }
+        }
+        private void movePlayer2()
+        { 
+            for (int i = snake2.Count - 1; i >= 0; i--)
+            {
+                // move the head.
+                if (i == 0)
+                {
+                    switch (SettingsP2.direction)
+                    {
+                        case Direction.Right:
+                            snake2[i].x++;
+                            break;
+                        case Direction.Left:
+                            snake2[i].x--;
+                            break;
+                        case Direction.Up:
+                            snake2[i].y--;
+                            break;
+                        case Direction.Down:
+                            snake2[i].y++;
+                            break;
+                    }
+                    // get max X and Y positions.
+                    int maxXPos = (picCanvas.Size.Width / SettingsP2.width);
+                    int maxYPos = (picCanvas.Size.Height / SettingsP2.height);
+                    // detect collision with game boundaries.
+                    if (snake2[i].x < 0 || snake2[i].y < 0
+                        || snake2[i].x >= maxXPos || snake2[i].y >= maxYPos)
+                    {
+                        die();
+                    }
+                    // detect collision with body.
+                    for (int j = 1; j < snake2.Count; ++j)
+                    {
+                        if ((snake2[i].x == snake2[j].x && snake2[i].y == snake2[j].y) )
+                        {
+                            die();
+                        }
+                    }
+                    for (int k = 1; k < snake.Count; ++k)
+                    {
+                        if ((snake2[0].x == snake[k].x && snake2[0].y == snake[k].y))
+                        {
+                            die();
+                        }
+                    }
+                    // detect collision with food.
+                    if (snake2[i].x == food.x && snake2[i].y == food.y)
+                    {
+                        eat2();
+                    }
+                }
+                // move the rest of the body.
+                else
+                {
+                    // set current node to previous node's position.
+                    snake2[i].x = snake2[i - 1].x;
+                    snake2[i].y = snake2[i - 1].y;
+                }
+
+            }
+        }
+
+        private void eat2()
+        {
+            // add circle to the body.
+            Circle food = new Circle();
+            food.x = snake2[snake2.Count - 1].x;
+            food.y = snake2[snake2.Count - 1].y;
+            snake2.Add(food);
+            // update score
+            SettingsP2.score += SettingsP2.points;
+            // create a new food object.
+            generateFood();
+        }
+        private void eat()
+        {
+            // add circle to the body.
+            Circle food = new Circle();
+            food.x = snake[snake.Count - 1].x;
+            food.y = snake[snake.Count - 1].y;
+            snake.Add(food);
+            // update score
+            Settings.score += Settings.points;
+            // create a new food object.
+            generateFood();
+        }
+        private void die()
+        {
+            Settings.gameOver = true;
+            tmrTimer.Tick -= updateScreen;
+            tmrTimer2.Tick -= updateScreen2;
+            tmrTimer.Stop();
+            tmrTimer2.Stop();
+            btnPlay.Enabled = true;
+        }
+
+        private void frmGame_KeyDown(object sender, KeyEventArgs e)
+        {
+            Input.ChangeState(e.KeyCode, true);
+        }
+
+        private void frmGame_KeyUp(object sender, KeyEventArgs e)
+        {
+            Input.ChangeState(e.KeyCode, false);
+        }
+
+        private void btnPlay_Click(object sender, EventArgs e)
+        {
+            Focus();
+            startGame();
+        }
+
+        private void frmGame_Load(object sender, EventArgs e)
+        {
+            btnPlay.Text = "Retry";
+            tmrTimer.Enabled = false;
+            tmrTimer2.Enabled = false; 
+        }
+
+        private void aboutToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            string msg = "Tran Bao Ngoc, ";
+            msg += "30/3/2022.";
+            msg += "\nCoded in C# in Visual Studio.";
+            MessageBox.Show(msg, "About", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void newGameToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            btnPlay_Click(sender, e);
+        }
+    }
+}
